a0d25a041881766f10d7a6549caf16ae688dbc2a
akalaC <100920662+akalaC@users.noreply.github.com>
2022-03-08 19:17:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+num1 = input("enter a number: ")
+num2 = input("enter another number: ")
+
+
+print("1 = add")
+print("2 = substract")
+print("3 = multiply")
+print("4 = divide")
+
+
+chosen = input("chose one of the above: ")
+
+num1_float = float(num1)
+num2_float = float(num2)
+chosen_float = float(chosen)
+
+if chosen_float == 1:
+    print(num1_float + num2_float)
+elif chosen_float == 2:
+    print(num1_float - num2_float)
+elif chosen_float == 3:
+    print(num1_float * num2_float)
+elif chosen_float == 4:
+    print(num1_float / num2_float)
+else:
+    print("An error occured, please try again")
+
